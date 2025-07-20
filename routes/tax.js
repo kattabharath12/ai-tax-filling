f7a2b4ddbc843ab08323412622d038707d1980a6
kattabharath12 <106069483+kattabharath12@users.noreply.github.com>
2025-07-20 16:16:53 -0500

    Update tax.js

--- routes/tax.js
+++ routes/tax.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
-const User = require('../models/User');
+const { User } = require('../database');
 const auth = require('../middleware/auth');
 const { Op } = require('sequelize');
 
