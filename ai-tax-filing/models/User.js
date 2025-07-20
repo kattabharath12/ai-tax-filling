5f73227c8a8c89f8a8b06d80c8010ab33b5959ce
kattabharath12 <106069483+kattabharath12@users.noreply.github.com>
2025-07-20 11:44:14 -0500

    Update User.js

--- ai-tax-filing/models/User.js
+++ ai-tax-filing/models/User.js
@@ -1,7 +1,7 @@
 const { DataTypes, Sequelize } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
-// Use DATABASE_URL from Railway
+// Use Railway's DATABASE_URL
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
@@ -20,7 +20,7 @@ const User = sequelize.define('User', {
     primaryKey: true
   },
   email: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(255),
     allowNull: false,
     unique: true,
     validate: {
@@ -28,65 +28,53 @@ const User = sequelize.define('User', {
     }
   },
   password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [6, 255]
-    }
+    type: DataTypes.STRING(255),
+    allowNull: false
   },
   firstName: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(100),
     allowNull: false
   },
   lastName: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(100),
     allowNull: false
   },
   phone: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(20),
     allowNull: true
   },
   taxInfo: {
     type: DataTypes.JSONB,
-    defaultValue: {
-      filingStatus: 'single',
-      dependents: [],
-      address: {}
-    }
+    defaultValue: {},
+    allowNull: false
   },
   documents: {
     type: DataTypes.JSONB,
-    defaultValue: []
+    defaultValue: [],
+    allowNull: false
   },
   taxReturn: {
     type: DataTypes.JSONB,
-    defaultValue: {
-      form1040: null,
-      status: 'draft',
-      submissionDate: null
-    }
+    defaultValue: {},
+    allowNull: false
   },
   payments: {
     type: DataTypes.JSONB,
-    defaultValue: []
+    defaultValue: [],
+    allowNull: false
   }
 }, {
+  tableName: 'Users',
   timestamps: true,
   hooks: {
     beforeCreate: async (user) => {
       if (user.password) {
         user.password = await bcrypt.hash(user.password, 12);
       }
-    },
-    beforeUpdate: async (user) => {
-      if (user.changed('password')) {
-        user.password = await bcrypt.hash(user.password, 12);
-      }
     }
   }
 });
 
-// Instance method to compare password
 User.prototype.comparePassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
